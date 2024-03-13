e62f5ad6525fb16c336c9c1d3a751013518db442
Bipul kumar <bipulsinghnadsena005@gmail.com>
2024-03-13 18:25:48 +0530

    commit

--- routes/Social_Feed.js
+++ routes/Social_Feed.js
@@ -3,4 +3,5 @@ const router= express.Router()
 const { auth }= require("../middleware/auth")
 const {getSocialFeed}= require("../controllers/Social_Feed")
 router.get("/getSocialFeed",auth,getSocialFeed)
+
 module.exports = router;
