d91a5aed9f4e7ae739b2e06d57c160f0cc468e4b
Bipul kumar <94814946+Bipul005@users.noreply.github.com>
2024-03-16 09:50:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -56,11 +56,7 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/hii", (req, res) => {
-  res.status(200).json({
-    message: "come to the API",
-  });
-});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
